63fdd5978798c55625b299b999822a5d7c1d2079
Aleksandr Chuklin <a.chuklin@gmail.com>
2016-08-03 10:32:27 +0200

    Add license and contributing files. Add license header and brief descriptions to the source files

--- logs_processing/remove_unused_css_classes.py
+++ logs_processing/remove_unused_css_classes.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python
-
-# Read the CSS file line by line and remove rules that only include classes that we are
-# not interested in. This is to reduce the size of a CSS file to squeeze it into CrowdFlower.
 #
-# IMPORTANT! CSS file is assumed to be beutified by http://codebeautify.org/css-beautify-minify.
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+#
+# Read the CSS file line by line and remove rules that only include classes
+# that we are not interested in.
+# This is to reduce the size of a CSS file to squeeze it into CrowdFlower's limits.
+#
+# IMPORTANT! CSS file is assumed to be beautified by 
+# http://codebeautify.org/css-beautify-minify.
 
 import sys
 from cStringIO import StringIO
